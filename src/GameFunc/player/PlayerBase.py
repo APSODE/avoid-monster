f3e5e06c02a498b80ca7d3c9eebf68b463bcdaca
APSODE <2gunbo74@gmail.com>
2023-01-14 14:25:55 +0900

    플레이어 관련 super class 생성

---
+++ src/GameFunc/player/PlayerBase.py
@@ -0,0 +1,10 @@
+from typing import Dict, List
+
+
+class PlayerBase:
+    def __init__(self):
+        pass
+        # self._element =
+        # self._equipment =
+        # self._skill =
+        # self._status =
