7211ca3de9fd5aea73103b056499c35f1a781293
APSODE <2gunbo74@gmail.com>
2023-01-18 18:15:25 +0900

    시작 메뉴에서 continue값이 잘못 전달되어 작동하던 문제 수정

--- src/GameFunc/display/DisplayManager.py
+++ src/GameFunc/display/DisplayManager.py
@@ -6,7 +6,8 @@ class DisplayManager:
     def __init__(self, gui_data: Dict[str, Union[int, bool]], root_path: str):
         self._display_data_object = DisplayData(
             screen_res = DisplayResolution.HD if gui_data.get("sc_res") == "hd" else DisplayResolution.FHD,
-            screen_img_dir = f"{root_path}\\resources\\1920_1080_space.jpg" #테스트용 background image
+            screen_img_dir = f"{root_path}\\resources\\1920_1080_space.jpg", #테스트용 background image
+            cont = gui_data.get("continue")
         )
 
     @property
