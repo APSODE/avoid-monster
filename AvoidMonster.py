a4db2fac47b03f08c2a9f5a2b44c8f4008180e71
APSODE <2gunbo74@gmail.com>
2023-01-19 21:57:39 +0900

    Manager들을 ObjectContainer에서 호출하는것으로 변경함에 따른 코드 수정

--- AvoidMonster.py
+++ AvoidMonster.py
@@ -1,5 +1,3 @@
-from src.GameFunc.settings.SettingManager import SettingManager
-from src.GameFunc.event.EventHandler import EventHandler
 from src.GameFunc.CustomException.AvoidMonster_CE import GameQuitException
 from src.GameFunc.player.TestPlayer import TestPlayer
 
@@ -12,43 +10,47 @@ PROJECT_ROOT_PATH = os.path.dirname(os.path.abspath(__file__)) #프로젝트 루
 
 class AvoidMonster:
     def __init__(self):
+        # Circular Import Exceprion을 방지하기 위하여 ObjectContainer의 import 타임을 runtime때 호출되도록 변경
+        from src.GameFunc.container.ObjectContainer import ObjectContainer
         self._work_status = True
-        self._setting_manager = SettingManager(root_path = PROJECT_ROOT_PATH)
+        self._object_container = ObjectContainer(root_path = PROJECT_ROOT_PATH)
         self._screen: pygame.Surface
 
     def StartGame(self) -> None:
         # print(f"continue : {self._setting_manager.DisplayManager.DisplayData.ContinueInfo}")
-        if self._setting_manager.DisplayManager.DisplayData.ContinueInfo is False:
+        if self._object_container.DisplayManager.DisplayData.ContinueInfo is False:
             return None
         else:
             pygame.init()
             self._ApplyGameSetting()
 
-            player = TestPlayer()
+            player = TestPlayer(object_name = "test_player_object")
             player.MoveData.X_Pos = 250 # 테스트용 위치
             player.MoveData.Y_Pos = 250 # 테스트용 위치
+
+            self._object_container.PlayerManager.AddPlayerObjectToContainer(player_object = player)
+            player = self._object_container.PlayerManager.PlayerContainer.get("test_player_object")
 
             while self._work_status:
                 for occurred_event in pygame.event.get():
                     try:
+                        # Circular Import Exceprion을 방지하기 위하여 EventHandler의 import 타임을 runtime때 호출되도록 변경
+                        from src.GameFunc.event.EventHandler import EventHandler
                         EventHandler(
                             event = occurred_event,
-                            object_dict = {
-                                "player": player,
-                                "display": self._setting_manager.DisplayManager.DisplayData
-                            }
+                            object_container = self._object_container
                         )
 
                     except GameQuitException:
                         self._work_status = False
 
-                self._screen.blit(self._setting_manager.DisplayManager.DisplayData.ScreenImage, (0, 0))
-                self._screen.blit(player.PlayerSprite.sprite, (player.MoveData.X_Pos, player.MoveData.Y_Pos))
+                self._screen.blit(self._object_container.DisplayManager.DisplayData.ScreenImage, (0, 0))
+                self._screen.blit(player.Sprite.sprite_sf, (player.MoveData.X_Pos, player.MoveData.Y_Pos))
                 pygame.display.update()
 
     def _ApplyGameSetting(self) -> None:
-        self._screen = self._setting_manager.ApplySetting_display()
-        SettingManager.ApplySetting_key()
+        self._screen = self._object_container.SettingManager.ApplySetting_display()
+        self._object_container.SettingManager.ApplySetting_key()
 
 
 if __name__ == '__main__':
