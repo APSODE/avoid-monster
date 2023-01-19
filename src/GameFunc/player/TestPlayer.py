3d4be200f8e251d8830c212515cb046bb5c30829
APSODE <2gunbo74@gmail.com>
2023-01-19 21:56:29 +0900

    PlayerSprite를 Player클래스와 분리

--- src/GameFunc/player/TestPlayer.py
+++ src/GameFunc/player/TestPlayer.py
@@ -1,22 +1,6 @@
 from src.GameFunc.player.PlayerBase import PlayerBase
-import pygame
-
-
-class PlayerSprite(pygame.sprite.Sprite):
-    def __init__(self):
-        super().__init__()
-        # self.sprite = pygame.image.load("D:\\PROJECT\\Python\\AvoidMonster\\참고용\\pygame\\Class\\C_UNKNOWN_CH\\IMG\\CHAR_3.png")
-        self.sprite = pygame.image.load(".\\resources\\CHAR_3.png")
-        self.size = self.sprite.get_size()
 
 
 class TestPlayer(PlayerBase):
-    def __init__(self):
-        super().__init__()
-        self._player_sprite = PlayerSprite()
-        
-    @property
-    def PlayerSprite(self):
-        return self._player_sprite
-
-
+    def __init__(self, object_name: str):
+        super().__init__(obj_name = object_name)
