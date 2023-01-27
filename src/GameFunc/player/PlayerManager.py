c93e0e8842980d0cef6a95ff170992c359b63937
APSODE <2gunbo74@gmail.com>
2023-01-27 20:46:16 +0900

    애니메이션 구현을 위한 스프라이트 업데이트 코드 (수정필요)

--- src/GameFunc/player/PlayerManager.py
+++ src/GameFunc/player/PlayerManager.py
@@ -15,7 +15,7 @@ class PlayerManager:
         return self._player_object
 
     @staticmethod
-    def MovePlayer(player_object: Optional[TestPlayer], direction: DirectionEnum, display_res: DisplayResolution):
+    def MovePlayer(player_object: Optional[PlayerBase], direction: DirectionEnum, display_res: DisplayResolution):
         # player의 이동후 좌표 = <player의 speed> * <player의 이동방향에 따른 x,y좌표 연산자(-1, 0, 1)> + <player의 현재 좌표>
         player_speed = player_object.MoveData.Speed
 
@@ -29,6 +29,7 @@ class PlayerManager:
         y_pos_check = 0 <= player_y_pos + player_mv_y <= display_res.height - player_object.Sprite.size[1] # 스크린 사이즈 경계 침범 체크
 
         if x_pos_check and y_pos_check:
+            player_object.Sprite.WalkAnimation() #TODO 플레이어 애니메이션을 한곳에서 관리하도록 해야함
             player_object.MoveData.X_Move = player_speed * direction.x_op
             player_object.MoveData.Y_Move = player_speed * direction.y_op
 
