0c81d3ff8040f77cbd36c44c823f58a0f1442d3a
APSODE <2gunbo74@gmail.com>
2023-01-27 20:36:16 +0900

    애니메이션에 사용되는 스프라이트를 프레임별로 분리하여 저장하는 코드 작성

--- src/tools/ImageEditor.py
+++ src/tools/ImageEditor.py
@@ -1,22 +1,60 @@
-from PIL import Image
-from src.decorator.ErrorChecker import ErrorChecker
-from typing import Tuple
+from typing import *
+from PIL import Image as img
+from PIL.Image import Image
+
 
 class ImageEditor:
-    @staticmethod
-    def EditSize(file_dir: str, size: Tuple[int, int]) -> Image:
-        target_image = Image.open(file_dir)
-        return target_image.resize(size = size)
+    def __init__(self, image_dir: str):
+        self._target_image = img.open(image_dir)
 
+    def EditSize(self, size: Tuple[int, int]) -> Image:
+        return self._target_image.resize(size = size)
 
+    def Crop(self, part_amount: Tuple[int, int]) -> List[Image]:
+        rt_image_list = []
 
+        target_image_size = self._target_image.size
 
+        part_width = target_image_size[0] // part_amount[0]
+        part_height = target_image_size[1] // part_amount[1]
+
+        result_pos_data = {
+            "x1": 0,
+            "y1": 0,
+            "x2": part_width,
+            "y2": part_height
+        }
+
+        for _ in range(part_amount[1]): #height
+            for _ in range(part_amount[0]): #width
+                rt_image_list.append(
+                    self._target_image.crop(
+                        box = (
+                            result_pos_data.get("x1"),
+                            result_pos_data.get("y1"),
+                            result_pos_data.get("x2"),
+                            result_pos_data.get("y2")
+                        )
+                    )
+                )
+
+                result_pos_data["x1"] += part_width
+                result_pos_data["x2"] += part_width
+            result_pos_data["y1"] += part_height
+            result_pos_data["y2"] += part_height
+
+        return rt_image_list
 
 
 if __name__ == '__main__':
-    edited_image = ImageEditor.EditSize(
-        file_dir = "..\\..\\resources\\start_menu.png",
-        size = (480, 320)
+    IE = ImageEditor(
+        image_dir = "..\\..\\스프라이트\\idle sheet-Sheet.png"
     )
-    edited_image.save("..\\..\\resources\\start_menu (edited).png")
+
+    # IE.EditSize(size = (100, 100)).save("xptmxm.png")
+    count = 1
+    for edited_image in IE.Crop(part_amount = (18, 1)):
+        edited_image.save(f"..\\..\\resources\\player\\standing\\standing_{count}.png")
+        count += 1
+
 
