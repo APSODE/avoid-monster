b9729f45ab1a89118ac032e416ddd46cab8203eb
APSODE <2gunbo74@gmail.com>
2023-01-27 20:44:23 +0900

    세팅 데이터 적용 기능의 구현 수정

--- src/GameFunc/settings/SettingManager.py
+++ src/GameFunc/settings/SettingManager.py
@@ -1,16 +1,23 @@
 from src.GameFunc.settings.SettingDataObject import SettingDataObject
 from src.GameFunc.display.DisplayManager import DisplayManager
-from src.SettingGUI.SettingGUI_qt import SettingGUI
+from typing import *
+
 import pygame
+
 
 class SettingManager:
     def __init__(self, dp_manager: DisplayManager):
         self._display_manager = dp_manager
-
+        self._screen = self.ApplySetting_display()
+        self.ApplySetting_key()
 
     @property
     def DisplayManager(self) -> DisplayManager:
         return self._display_manager
+
+    @property
+    def Screen(self) -> pygame.Surface:
+        return self._screen
 
     def ApplySetting_display(self, setting_data: SettingDataObject = None) -> pygame.Surface:
         screen_res = (
