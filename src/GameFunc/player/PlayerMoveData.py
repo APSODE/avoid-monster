0822f00be1e66e960fa6b84b60a0b4f8eba9117f
APSODE <2gunbo74@gmail.com>
2023-01-27 20:42:14 +0900

    DirectionEnum의 분리에 따른 import문 추가

--- src/GameFunc/player/PlayerMoveData.py
+++ src/GameFunc/player/PlayerMoveData.py
@@ -1,27 +1,5 @@
-from typing import Dict, Optional
-from enum import Enum
-
-
-class DirectionEnum(Enum):
-    RIGHT = ("right", 1, 0) # (name, x_operator, y_operator)
-    LEFT = ("left", -1, 0)
-    UP = ("up", 0, -1)
-    DOWN = ("down", 0, 1)
-    UP_RIGHT = ("up_right", 1, -1)
-    UP_LEFT = ("up_left", -1, -1)
-    DOWN_RIGHT = ("down_right", 1, 1)
-    DOWN_LEFT = ("down_left", -1, 1)
-    NONE = ("none_direction", 0, 0)
-
-    def __init__(self, direction: str, x_op: int, y_op: int):
-        """
-        :param direction:  방향에 대한 문자열 정보
-        :param x_op:  방향에 따른 x좌표 연산에 사용되는 연산자
-        :param y_op:  방향에 따른 y좌표 연산에 사용되는 연산자
-        """
-        self.direction_str = direction
-        self.x_op = x_op
-        self.y_op = y_op
+from typing import *
+from src.GameFunc.enum.AvoidMoster_Enum import DirectionEnum
 
 
 class PlayerMoveData:
