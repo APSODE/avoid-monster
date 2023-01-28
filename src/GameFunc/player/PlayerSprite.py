c89d33da9ec855b40c7c72ae886e1fc2cf9f78e0
APSODE <2gunbo74@gmail.com>
2023-01-29 03:00:43 +0900

    플레이어의 정지&움직임 상태의 애니메이션을 위한 스프라이트 업데이트 기능 구현

--- src/GameFunc/player/PlayerSprite.py
+++ src/GameFunc/player/PlayerSprite.py
@@ -1,60 +1,55 @@
 from typing import *
 import pygame
 
+from src.GameFunc.player.PlayerMoveData import PlayerMoveData
+
 
 class PlayerSprite(pygame.sprite.Sprite):
     def __init__(self):
         super().__init__()
-        # self.sprite = pygame.image.load("D:\\PROJECT\\Python\\AvoidMonster\\참고용\\pygame\\Class\\C_UNKNOWN_CH\\IMG\\CHAR_3.png")
-        self.sprite_sf = pygame.image.load(".\\resources\\CHAR_3.png") # sprite surface
+        self.sprite_sf = pygame.image.load(".\\resources\\CHAR_3.png")  # sprite surface
         self.size = self.sprite_sf.get_size()
-        self._standing_sprite_num = 1
-        self._walk_sprite_num = 1
-        self._last_update = 0
+        self._sprite_num_data = {
+            "s": 1,  # 멈춰있는 상태의 스프라이트 이미지 번호
+            "w": 1,  # 움직이는 상태의 스프라이트 이미지 번호
+            "s_M": 18,  # 멈춰있는 상태의 스프라이트 이미지의 최대 번호
+            "w_M": 24  # 움직이는 상태의 스프라이트 이미지의 최대 번호
+        }
+        self._last_update_tick = 0
+        self._update_delay = 25
+        self._resources_dir = ".\\resources\\player\\"
 
-    def StandingAnimation(self, update_delay: int = 25): #TODO 플레이어 애니메이션을 한곳에서 관리하도록 해야함
-        now_game_tick = pygame.time.get_ticks()  # 단위 ms(밀리세컨드)
+    def _SpriteUpdateTimeCheck(self, now_tick: int) -> bool:
+        return now_tick - self._last_update_tick > self._update_delay
 
-        if self._standing_sprite_num >= 19:
-            self._standing_sprite_num = 1
+    def UpdateSprite(self, move_data: PlayerMoveData, update_delay: int = 25):  # 25ms(단위 : 밀리세컨드)
+        if self._update_delay != update_delay:
+            self._update_delay = update_delay
 
-        update_tick_check = now_game_tick - self._last_update > update_delay  # 100밀리초마다 이미지 갱신
+        if move_data.MovingStatus:  #움직임 여부 확인
+            current_sprite_num_key = "w"
+            max_sprite_num_key = "w_M"
+            resource_dir = self._resources_dir + "move\\walk_"
 
-        print(
-            f"update tick check : {update_tick_check}\n"
-            f"now tick : {now_game_tick}ms\n"
-            f"last update : {self._last_update}ms\n\n"
-        )
-        if update_tick_check:
-            self.sprite_sf = pygame.image.load(
-                f".\\resources\\player\\standing\\standing_{self._standing_sprite_num}.png"
+        else:
+            current_sprite_num_key = "s"
+            max_sprite_num_key = "s_M"
+            resource_dir = self._resources_dir + "standing\\standing_"
+
+        if self._sprite_num_data.get(current_sprite_num_key) > self._sprite_num_data.get(max_sprite_num_key):
+            self._sprite_num_data[current_sprite_num_key] = 1
+
+        now_game_tick = pygame.time.get_ticks()
+
+        if self._SpriteUpdateTimeCheck(now_tick=now_game_tick):
+            resource_dir += f"{self._sprite_num_data.get(current_sprite_num_key)}.png"
+            current_sprite_image = pygame.image.load(resource_dir)
+            self.sprite_sf = pygame.transform.flip(
+                surface = current_sprite_image,
+                flip_x = False if move_data.X_Move > 0 else True,
+                flip_y = False
             )
 
-            self._standing_sprite_num += 1
-            self._last_update = now_game_tick
-
-    # 플레이어 이동 키이벤트가 발생했을때만 작동
-    def WalkAnimation(self, update_delay: int = 100): #TODO 플레이어 애니메이션을 한곳에서 관리하도록 해야함
-        now_game_tick = pygame.time.get_ticks() #단위 ms(밀리세컨드)
-
-        if self._walk_sprite_num >= 25: #플레이어 애니메이션 스프라이트 갯수를 초과확인용
-            self._walk_sprite_num = 1
-
-        update_tick_check = now_game_tick - self._last_update > update_delay # 100밀리초마다 이미지 갱신
-        print(
-            f"update tick check : {update_tick_check}\n"
-            f"now tick : {now_game_tick}ms\n"
-            f"last update : {self._last_update}ms\n\n"
-        )
-        if update_tick_check:
-            self.sprite_sf = pygame.image.load(f".\\resources\\player\\move\\walk_{self._walk_sprite_num}.png")
-
-            self._walk_sprite_num += 1
-            self._last_update = now_game_tick
-
-
-
-
-
-
+            self._sprite_num_data[current_sprite_num_key] += 1
+            self._last_update_tick = now_game_tick
 
