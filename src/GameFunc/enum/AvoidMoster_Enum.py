3f7dfba664714d45ef45add7ebcf85542b66975f
APSODE <2gunbo74@gmail.com>
2023-01-31 00:43:57 +0900

    플레이어 스프라이트의 8방향 이동 애니메이션 지원을 위한 스프라이트 회전 각도 추가

--- src/GameFunc/enum/AvoidMoster_Enum.py
+++ src/GameFunc/enum/AvoidMoster_Enum.py
@@ -2,23 +2,25 @@ from enum import Enum
 
 
 class DirectionEnum(Enum):
-    RIGHT = ("right", 1, 0)  # (name, x_operator, y_operator)
-    LEFT = ("left", -1, 0)
-    UP = ("up", 0, -1)
-    DOWN = ("down", 0, 1)
-    UP_RIGHT = ("up_right", 1, -1)
-    UP_LEFT = ("up_left", -1, -1)
-    DOWN_RIGHT = ("down_right", 1, 1)
-    DOWN_LEFT = ("down_left", -1, 1)
-    NONE = ("none_direction", 0, 0)
+    RIGHT = ("right", 1, 0, 0)  # (name, x_operator, y_operator)
+    LEFT = ("left", -1, 0, 0)
+    UP = ("up", 0, -1, 90)
+    DOWN = ("down", 0, 1, 270)
+    UP_RIGHT = ("up_right", 1, -1, 45)
+    UP_LEFT = ("up_left", -1, -1, 315)
+    DOWN_RIGHT = ("down_right", 1, 1, 315)
+    DOWN_LEFT = ("down_left", -1, 1, 45)
+    NONE = ("none_direction", 0, 0, 0)
 
-    def __init__(self, direction: str, x_op: int, y_op: int):
+    def __init__(self, direction: str, x_op: int, y_op: int, rt_ag: int):
         """
         :param direction:  방향에 대한 문자열 정보
         :param x_op:  방향에 따른 x좌표 연산에 사용되는 연산자
         :param y_op:  방향에 따른 y좌표 연산에 사용되는 연산자
+        :param rt_ag:  방향에 따른 스프라이트 회전 각도
         """
         self.direction_str = direction
         self.x_op = x_op
         self.y_op = y_op
+        self.rt_ag = rt_ag
 
